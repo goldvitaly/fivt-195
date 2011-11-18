72241388c3c6ab3049e86109ab6c0b14cdacce8f
Alexey Dmitriev <riadwaw@gmail.com>
2011-11-18 21:55:43 +0400

    added test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include "IntervalTree.h"
-
+#include <cstdlib>
 using namespace std;
-struct s{
-	bool operator()(int a,int b){
-		return true;
+typedef long long LL;
+struct addSegment{
+	bool operator()(LL old, LL mod, LL len){
+		return old+mod*len;
 	}
 };
+void test_sum(int n,int times){
+	srand(1);
+	IntervalTree<LL,LL,std::plus<LL>,addSegment,std::plus<LL> > tree(n);
+	vector<LL> v(n);
+	for(int i=0;i<times;++i){
+		int l=rand()%n;
+		int r=rand()%n;
+		int value=rand();
+		if(l>r)
+			swap(l, r);
+		for(int i=l;i<=r;++i){
+			v[i] += value;
+		}
+		tree.set(l, r, value);
+
+		l=rand()%n;
+		r=rand()%n;
+		if(l>r)
+			swap(l, r);
+
+		LL ans = 0;
+
+		for(int i=l;i<=r;++i){
+			ans+=v[i];
+		}
+		assert(ans == tree.get(l,  r));
+
+	}
+}
 int main(int argc, char** argv) {
-	//
+	test_sum(1000,1000);
 	return 0;
 }
 
