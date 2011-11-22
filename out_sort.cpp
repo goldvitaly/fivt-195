fc43b72a1e7d354516ec087e54054a3762ac1bca
Yana Kashinskay <kashin.yana@gmail.com>
2011-11-23 00:19:18 +0400

    renaming functions and variables

--- out_sort.cpp
+++ out_sort.cpp
@@ -25,51 +25,46 @@ public:
 
 
 template<class Cmp>
-class Cmp_pair
+class CmpPairFirst
 {
 public:
     template<class T1, class T2>
-    bool operator () (pair<T1, T2> a, pair<T1, T2> b)
+    bool operator () (const pair<T1, T2>& a, const pair<T1, T2>& b)
     {
         return Cmp()(a.first, b.first);
     }
 };
 
 template<class T, class Cmp>
-void out_sort(ifstream& from, ofstream& rez, int size_bl = 1000, Cmp cmp)
+void out_sort(ifstream& from, ofstream& rez, int size_blok, Cmp cmp)
 {
     int num_bloks = 0;
-    char name_fail[20];
-    vector <T> A(size_bl);
+    char name_file[20];
+    vector <T> A(size_blok);
     while(!from.eof())
     {
-        sprintf(name_fail,"%d.txt", num_bloks);
-        ofstream to(name_fail);
-        int size_fail;
-        for(size_fail = 0; !from.eof() && size_fail < size_bl; size_fail++)
+        sprintf(name_file,"%d.txt", num_bloks);
+        ofstream to(name_file);
+        int size_file;
+        for(size_file = 0; !from.eof() && size_file < size_blok; size_file++)
         {
             T num_now;
             from >> num_now;
-            A[size_fail] = num_now;
+            A[size_file] = num_now;
         }
-        sort(A.begin(), A.begin() + size_fail, Cmp());
+        sort(A.begin(), A.begin() + size_file, Cmp());
 
-        for(int j  = 0; j < size_fail; j++)
+        for(int j  = 0; j < size_file; j++)
             to << A[j] << endl;
         num_bloks++;
     }
 
-    std::priority_queue <
-		std::pair < T, std::ifstream*>,
-		std::vector < std::pair < T, std::ifstream* > >,
-		Cmp_pair<ReverseCmp<Cmp> >
-	> Queue;
-
+    priority_queue<pair<T, ifstream*>, vector<pair<T, ifstream*> >,CmpPairFirst<ReverseCmp<Cmp> > > Queue;
 
     for(int i = 0; i < num_bloks; i++)
     {
-        sprintf(name_fail,"%d.txt", i);
-        ifstream* in = new ifstream(name_fail);
+        sprintf(name_file,"%d.txt", i);
+        ifstream* in = new ifstream(name_file);
         T num_now;
         (*in) >> num_now;
         Queue.push(make_pair(num_now, in));
@@ -79,7 +74,7 @@ void out_sort(ifstream& from, ofstream& rez, int size_bl = 1000, Cmp cmp)
         pair<T, ifstream*> Pair = Queue.top();
         Queue.pop();
         rez << Pair.first << endl;
-        if(!(*Pair.second).eof())
+        if(!Pair.second->eof())
         {
             T num_now;
             *(Pair.second) >> num_now;
