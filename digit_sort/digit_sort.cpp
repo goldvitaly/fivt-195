c0f4719738d1ae4f04ce8e56ede99087d14d8e2e
Aleksandr Timin <altimin@altimin.ru>
2011-11-30 16:12:12 +0400

    some fixes

--- digit_sort/digit_sort.cpp
+++ digit_sort/digit_sort.cpp
@@ -1,43 +1,43 @@
 #include <vector>
 #include <algorithm>
-#include <string.h>
+#include <string>
 #include <time.h>
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <limits> 
+#include <numeric>
 
-class int_extractor
+namespace math
 {
-	static const int bits = 32;
-	const int block_size;
-	const int max_blocks;
-public:
-	int_extractor(int block_size_ = 8): block_size(block_size_), max_blocks(bits / block_size_ + bool(bits % block_size_)) { };
-	inline int get_block_size() const  { return block_size; };
-	inline int blocks(const int& a) const { return max_blocks; }
-	inline int operator ()(const int& a, const int& block_number, const int& blocks) const { return (a >> (block_size * block_number)) & ((1 << block_size) - 1); };
+	template <class T> T divide_and_round_up(T a, T b) { return a / b + bool(a % b); };
 };
 
 
-class long_long_extractor
+
+template <class T>
+class integral_extractor
 {
-	static const int bits = 64;
-	const int max_blocks;
+	static const int bits = sizeof(T) * 8; // or numeric_limits<T>::digits ?;
 	const int block_size;
+	const int max_blocks;
 public:
-	long_long_extractor(int block_size_ = 8): block_size(block_size_), max_blocks(bits / block_size_ + bool(bits % block_size_)) { };
-	int get_block_size() const { return block_size; };
-	int blocks(int a) const { return max_blocks; }
-	inline int operator ()(const long long& a, const long long& block_number, const int& blocks) const { return (a >> (block_size * block_number)) & ((1 << block_size) - 1); };
+	integral_extractor(int block_size_ = 8): block_size(block_size_), max_blocks(math::divide_and_round_up(bits, block_size_)) { };
+	int get_block_size() const  { return block_size; };
+	int blocks_in_element(T element) const { return max_blocks; }
+	int operator ()(T element, int block_number, int blocks) const 
+	{ 
+		return (element >> (block_size * block_number)) & ((1 << block_size) - 1); 
+	};
 };
 
 class string_extractor
 {
 	static const int block_size = 8;
 public:
-	int blocks(const std::string& s) const { return s.size(); };
+	int blocks_in_element(const std::string& element) const { return element.size(); };
 	int get_block_size() const { return block_size; };
-	inline int operator () (const std::string& s, int& block_number, const int& blocks) const
+	int operator () (const std::string& s, int& block_number, const int& blocks) const
 	{
 		if ((blocks - block_number - 1) >= s.size()) 
 			return 0;
@@ -58,35 +58,47 @@ class timer
 		{ 
 			start_time = clock(); 
 		}
-		double get_time() 
+		double get_time() const
 		{
 			return (clock() - start_time) / (double) CLOCKS_PER_SEC;
 		}
 };
 
 template <typename It, class BlockExtractor>
-void digit_sort(It l, It r, const BlockExtractor& extr)
+void digit_sort(It begin, It end, const BlockExtractor& extr)
 {
 	typedef typename std::iterator_traits<It>::value_type value_type;
 	typedef value_type* pvalue_type;
-	int size = r - l;
-	int count_size = (1 << extr.get_block_size()) + 1;
-	int blocks = 0;
-	for (int i = 0; i < size; i ++)
-		blocks = std::max(blocks, extr.blocks(*(l + i)));
+	int size = end - begin;
+	int number_of_blocks = 0;
+	for (int i = 0; i < size; i++)
+		number_of_blocks = std::max(number_of_blocks, extr.blocks_in_element(*(begin + i)));
 	std::vector <value_type> sorted_array(size);
-	std::vector <int> count(count_size);
-	for (int block_number = 0; block_number < blocks; block_number ++)
+	int count_size = (1 << extr.get_block_size()) + 1;
+	std::vector <unsigned int> count(count_size);
+	for (int block_number = 0; block_number < number_of_blocks; block_number++)
 	{
 		fill(count.begin(),count.end(),0);
-		for (It i = l; i != r; i ++)
-			count[extr(*i,block_number, blocks) + 1] ++;
-		for (int i = 0; i < count_size - 1; i ++)
+		for (It i = begin; i != end; i++)
+			count[extr(*i,block_number, number_of_blocks) + 1]++;
+		for (int i = 0; i < count_size - 1; i++)
 			count[i + 1] += count[i];
-		for (It i = l; i != r; i ++)
-			sorted_array[count[extr(*i, block_number, blocks)] ++] = *i;
-		copy(sorted_array.begin(),sorted_array.end(),l);
+		for (It i = begin; i != end; i++)
+		{
+			unsigned int& position = count[extr(*i, block_number, number_of_blocks)];
+			sorted_array[position++] = *i;
+		}
+		copy(sorted_array.begin(),sorted_array.end(),begin);
 	}
+};
+
+template <typename It>
+bool is_equal(It l, It r, int n)
+{
+	for (int i = 0; i < n; i ++) 
+		if (*(l + i) != *(r + i))
+			return false;
+	return true;
 };
 
 
@@ -94,69 +106,54 @@ template <typename It>
 bool is_sorted(It l, It r)
 {
 	It next = l; next++;
-	for (It i = l; next != r; next ++, i ++)
+	for (It i = l; next != r; next++, i++)
 		if (*next < *i)
 			return false;
 	return true;
 };
 
-template <typename It>
-bool is_equal(It first, It second, int n)
-{
-	for (int i = 0; i < n; i ++)
-		if (!(*(first + i) == *(second + i)))
-			return false;
-	return true;
-
-}
-
-
 void measure_int(int bits = 8, int n = 50000000)
 {
-	int* a = new int[n];
-	int* b = new int[n];
-	for (int i = 0; i < n; i ++)
+	std::vector<int> a(n);
+	std::vector<int> b(n);
+	for (int i = 0; i < n; i++)
 		a[i] = b[i] = rand();
 	timer t;
 	t.start();
-	std::sort(a,a + n);
+	std::sort(a.begin(),a.end());
 	double std_sort_time = t.get_time();
 	t.start();
-	digit_sort(b,b + n, int_extractor(bits));
+	digit_sort(b.begin(),b.end(), integral_extractor<int>(bits));
 	double digit_sort_time = t.get_time();
-	assert(is_sorted(b,b + n));
-	assert(is_equal(a,b,n));
+	assert(is_sorted(b.begin(),b.end()));
+	assert(std::equal(a.begin(),a.end(),b.begin()));
 	fprintf(stderr, "%d integers. Extracting %d bits. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, bits, std_sort_time, digit_sort_time, digit_sort_time/std_sort_time);
 	fprintf(stdout, "%d integers. Extracting %d bits. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, bits, std_sort_time, digit_sort_time, digit_sort_time/std_sort_time);
-	delete[] a;
-	delete[] b;
 }
 
 void measure_long_long(int bits = 8,int n = 50000000)
 {
-	long long* a = new long long[n];
-	long long* b = new long long[n];
-	for (int i = 0; i < n; i ++)
+	std::vector<long long> a(n);
+	std::vector<long long> b(n);
+	for (int i = 0; i < n; i++)
 		a[i] = b[i] = ((((long long)rand()) << 32LL) | ((long long)rand()));
 	timer t;
 	t.start();
-	std::sort(a,a + n);
+	std::sort(a.begin(),a.end());
 	double std_sort_time = t.get_time();
 	t.start();
-	digit_sort(b,b + n, long_long_extractor(bits));
+	digit_sort(b.begin(),b.end(), integral_extractor<long long>(bits));
 	double digit_sort_time = t.get_time();
-	assert(is_sorted(b,b + n));
-	assert(is_equal(a,b,n));
+	assert(is_sorted(b.begin(),b.end()));
+	assert(std::equal(a.begin(),a.end(), b.begin()));
 	fprintf(stderr, "%d long long integers. Extracting %d bits. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, bits, std_sort_time, digit_sort_time, digit_sort_time/std_sort_time);
 	fprintf(stdout, "%d long long integers. Extracting %d bits. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, bits, std_sort_time, digit_sort_time, digit_sort_time/std_sort_time);
-	delete[] a;
-	delete[] b;
 }
 
 std::string gen_string(int len)
 {
 	std::string res(len,0);
-	for (int i = 0; i < len; i ++)
+	for (int i = 0; i < len; i++)
 		res[i] = rand() % 128;
 	return res;
 }
@@ -165,7 +162,7 @@ void measure_string(int len = 6, int n = 50000000)
 {
 	std::string* a = new std::string[n];
 	std::string* b = new std::string[n];
-	for (int i = 0; i < n; i ++)
+	for (int i = 0; i < n; i++)
 		a[i] = b[i] = gen_string(len);
 	timer t;
 	t.start();
@@ -175,7 +172,7 @@ void measure_string(int len = 6, int n = 50000000)
 	std::sort(a,a + n);
 	double std_sort_time = t.get_time();
 	assert(is_sorted(b,b + n));
-	assert(is_equal(a,b,n));
+	assert(std::equal(a,b,a+n));
 	fprintf(stderr, "%d strings with length equal to %d. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, len, std_sort_time, digit_sort_time, digit_sort_time / std_sort_time);
 	fprintf(stdout, "%d strings with length equal to %d. std::sort works %.3lf seconds, my::digit_sort works %.3lf seconds. Factor is %.5lf\n", n, len, std_sort_time, digit_sort_time, digit_sort_time / std_sort_time);
 	delete[] a;
@@ -186,13 +183,7 @@ void measure_string(int len = 6, int n = 50000000)
 
 int main()
 {
-	measure_int(8,5000000);
-	return 0;
-	for (int i = 1; i <= 6; i ++)
-		measure_string(i);
-	measure_long_long();
-	measure_int();
-	for (int i = 1; i <= 24; i ++)
-		measure_int(i);
+	measure_int(8,500000);
+	measure_long_long(8,500000);
 	return 0;
 }
