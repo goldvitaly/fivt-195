a8176cec466074d74d8287681050725f7819cf07
Alexey Dmitriev <riadwaw@gmail.com>
2011-12-01 10:02:10 +0400

    using standart types more

--- testDigitSort.cpp
+++ testDigitSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "digit_sort.h"
 #include "StringSort.h"
@@ -8,36 +9,33 @@
 #include "generator.h"
 
 template<typename T, typename F, typename SortClass>
-void test(int n, F func, char * type, SortClass Sort) {
+void test(int n, F generator, std::string type, SortClass Sort) {
 	std::cout << n << " elements of Type " << type << std::endl;
-	T* arr = new T[n];
-	std::generate(arr, arr + n, func);
+	std::vector<T> arr(n);
+	std::generate(arr.begin(), arr.end(), generator);
 	clock_t start = clock();
-	std::sort(arr, arr + n);
+	std::sort(arr.begin(), arr.end());
 	std::cout << "STL: " << ((clock() - start) / 1000) << " ms" << std::endl;
 
-	std::generate(arr, arr + n, func);
+	std::generate(arr.begin(), arr.end(), generator);
 	start = clock();
-	digit_sort(arr, arr + n, Sort);
+	digit_sort(arr.begin(), arr.end(), Sort);
 	std::cout << "My: " << ((clock() - start) / 1000) << " ms" << std::endl;
-	if (std::is_sorted(arr, arr + n)) {
+	if (std::is_sorted(arr.begin(), arr.end())) {
 		std::cout << "Sorted correctly";
 	} else
 		std::cout << "Sorting INCORRECT!";
 
 	std::cout << std::endl << std::endl;
-
-	delete[] arr;
 }
 
 typedef unsigned long long ULL;
 void testAll() {
 	const int BYTE = 8;
-	test<unsigned>(500000, rand, (char*) "int", NumberSort(2, 2 * BYTE));
-	test<unsigned>(500000, rand, (char*) "int", NumberSort(4, BYTE));
-	test<ULL>(50000000, genLong, (char*) "long long", NumberSort(4, 2 * BYTE));
-	test<ULL>(50000000, genLong, (char*) "long long", NumberSort(8, BYTE));
-	test <std::pair<ULL,ULL>> (500000, genPair, (char*) "\"pair of long long\"", BigPairSort());
-	test<std::string> (30000, genString, (char*) "string", StringSort());
-
+	test<unsigned>(5000000, rand,"int", NumberSort(2, 2 * BYTE));
+	test<unsigned>(5000000, rand,"int", NumberSort(4, BYTE));
+	test<ULL>(5000000, genLong,"long long", NumberSort(4, 2 * BYTE));
+	test<ULL>(5000000, genLong,"long long", NumberSort(8, BYTE));
+	test <std::pair<ULL,ULL>> (500000, genPair, "\"pair of long long\"", BigPairSort());
+	test<std::string> (30000, genString,"string", StringSort());
 }
