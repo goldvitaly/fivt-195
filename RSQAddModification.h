09dfab35f439afc2955df26e259398565bc6bf5f
Alexey Dmitriev <riadwaw@gmail.com>
2011-12-05 22:07:56 +0400

    add forgotten file

---
+++ RSQAddModification.h
@@ -0,0 +1,31 @@
+#ifndef RSQADDMODIFICATION_H
+#define RSQADDMODIFICATION_H
+
+#include "IntervalTree.h"
+
+
+template<typename T>
+class RSQAddModification{
+	struct AddSegment{
+		T operator() (const T& val,const T& mod, size_t len) const{
+			return val+mod*len;
+		}
+	};
+	IntervalTree<T,T,std::plus<T>,AddSegment, std::plus<T> > tree;
+	public:
+	RSQAddModification(size_t n,T zero = T()):tree(n,zero){}
+
+	template<typename Iterator>
+	RSQAddModification(Iterator begin, Iterator end):tree(begin,end){}
+
+	T getSum(size_t l, size_t r){
+		return tree.get(l, r);
+	}
+
+	void increase(size_t l, size_t r, const T& value){
+		tree.set(l,r,value);
+	}
+};
+
+#endif
+
