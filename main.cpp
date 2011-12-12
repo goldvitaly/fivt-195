4c4f1741e58bb7d5ad5818ed9ec2993406e153f6
unknown <StelZ40494@StelZ40494-PC.(none)>
2011-12-12 20:56:46 +0300

    added pair & distance fix

--- main.cpp
+++ main.cpp
@@ -5,32 +5,20 @@
 #include <ctime>
 #include "interval_tree.h"
 
-
-
-
-
-class sum_int
+struct modify_int
 {
-    public:
-    int merge(int x, int y)
-    {
-        return (x + y);
-    }
-};
-
-class modify_int
-{
-    public:
-    int modify(int el, int mod, const size_t &range_size)
+    int operator()(int el, int mod, const size_t &range_size)
     {
         return (el + (mod * range_size));
     }
 };
 
-void generate_interval(int *left, int *right, size_t max)
+std::pair<int, int> generate_interval(size_t max)
 {
-    *right = rand() % max + 1;
-    *left = rand() % *right + 1;
+    std::pair<int, int> res;
+    res.second = rand() % max + 1;
+    res.first = rand() % res.second + 1;
+    return res;
 }
 
 
@@ -43,12 +31,15 @@ int main()
     std::cin >> number_of_elements >> number_of_tests;
     A.resize(number_of_elements, 0);
     S.resize(number_of_elements, 0);
-    int left, right, delta;
-    interval_tree<int, int, sum_int, modify_int, sum_int> tree1 = interval_tree<int, int, sum_int, modify_int, sum_int>(A.begin(), A.end(), 0);
+    int delta, left, right;
+    std::pair<int, int> seg;
+    interval_tree<int, int, std::plus<int>, modify_int, std::plus<int> > tree1 = interval_tree<int, int, std::plus<int>, modify_int, std::plus<int> >(A.begin(), A.end(), 0);
     bool fail = false;
     for(int i = 0; i < number_of_tests; i++)
     {
-        generate_interval(&left, &right, A.size());
+        seg = generate_interval(A.size());
+        left = seg.first;
+        right = seg.second;
         delta = rand() % 1000000;
         if((rand() & 1))    delta = -delta;
         tree1.mod_range(left, right, delta);
@@ -63,7 +54,9 @@ int main()
         }
         for(int j = right; j < S.size(); j++)
             S[j] = S[j - 1] + A[j];
-        generate_interval(&left, &right, A.size());
+        seg = generate_interval(A.size());
+        left = seg.first;
+        right = seg.second;
         delta = S[right - 1];              //we calculate partitial sum and push it in delta
         if(left - 1) delta -= S[left - 2];
         if(tree1.get_from_range(left, right) != delta)      //compare delta with interval_tree function
