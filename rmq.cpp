bf1e5512335843e5d5ea728447a0585c3a71f13f
Andrew Osipov <mekagem@gmail.com>
2011-12-12 11:14:35 +0400

    1

---
+++ rmq.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class IntervalTree
+{
+	
+};
+
+int main ()
+{
+	return 0;
+}
