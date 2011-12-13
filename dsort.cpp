d0c4f2a3f9ea4764e3937bcd23e84a80c0a9c5ea
MekaGem <mekagem@gmail.com>
2011-12-13 23:16:21 +0400

    Update dsort.cpp

--- dsort.cpp
+++ dsort.cpp
@@ -5,109 +5,81 @@
 #include <ctime>
 #include <algorithm>
 #include <cstdio>
+#include <numeric>
 
-using namespace std;
-
-//typedef string my_type;
-//const int SIZE = 10 * 1000 * 1000;
-
-class IKeyExtractor
+template <typename NumberType>
+class NumberKeyExtractor
 {
-public:
+private:
     size_t blockNum;
-    
-    IKeyExtractor(size_t KeyBlockNum = 2)
+public:   
+    explicit NumberKeyExtractor(size_t KeyBlockNum = 2)
     {
         blockNum = KeyBlockNum;
     }
-    size_t operator () (int Source, size_t KeyIndex)
+    size_t operator () (const NumberType &Source, size_t KeyIndex)const
     {
         unsigned short int *a = (unsigned short int *)(&Source);
         return *(a + KeyIndex);
     }
-};
-
-class LLKeyExtractor
-{
-public:
-    size_t blockNum;
-    
-    LLKeyExtractor(size_t KeyBlockNum = 4)
-    {
-        blockNum = KeyBlockNum;
-    }
-    size_t operator () (long long Source, size_t KeyIndex)
-    {
-        unsigned short int *a = (unsigned short int *)(&Source);
-        return *(a + KeyIndex);
-    }
+    size_t GetBlockNum ()const
+	{
+		return blockNum;
+	}
 };
 
 class SKeyExtractor
 {
-public:
+private:
     size_t blockNum;
-    
-    SKeyExtractor(size_t KeyBlockNum = 2)
+public:
+    explicit SKeyExtractor(size_t KeyBlockNum = 2)
     {
         blockNum = KeyBlockNum;
     }
-    size_t operator () (string &Source, size_t KeyIndex)
+    size_t operator () (const std::string &Source, size_t KeyIndex)const
     {
         return ((size_t)(Source[blockNum * 2 - 1 - KeyIndex * 2]) + (size_t(Source[blockNum * 2 - 1 - KeyIndex * 2 - 1]) << 8));
     }
+    size_t GetBlockNum ()const
+	{
+		return blockNum;
+	}
 };
 
-class PLLKeyExtractor
-{
-public:
-    size_t blockNum;
-    
-    PLLKeyExtractor(size_t KeyBlockNum = 8)
-    {
-        blockNum = KeyBlockNum;
-    }
-    size_t operator () (pair<long long, long long> Source, size_t KeyIndex)
-    {
-        unsigned short int *a = (unsigned short int *)(&Source);
-        return *(a + KeyIndex);
-    }
-};
-
-template <typename T, typename EXT>
-void DigitalSort (T Begin, T End, EXT KE )
+template <typename iterator, typename Extractor>
+void DigitalSort (iterator Begin, iterator End, Extractor KE )
 {
     const size_t KeyBlockSize = (1<<16), ArrayLength = End - Begin;
     
-    vector <__typeof(*Begin)> Buffer(ArrayLength);
-    vector <size_t> Num;
+    std::vector <__typeof(*Begin)> Buffer(ArrayLength);
+    std::vector <size_t> Num, StartIndex;
     
-    for (size_t Iteration = 0; Iteration < KE.blockNum; ++Iteration)
+    for (size_t Iteration = 0; Iteration < KE.GetBlockNum(); ++Iteration)
     {
         Num.resize(0);
         Num.resize(KeyBlockSize, 0);
         
-        for (T Iterator = Begin; Iterator != End; ++Iterator)
+        for (iterator Iterator = Begin; Iterator != End; ++Iterator)
         {
             Num[KE(*Iterator, Iteration)]++;
         }
         
+        //std::partial_sum(Num.begin(), Num.end() - 1, Num.begin() + 1);
+        //Не работает, а использовать лишнюю линюю памяти я не хочу.
         size_t Sum = 0;
         for (size_t i = 0; i < KeyBlockSize; ++i)
         {
-            swap( Sum, Num[i] );
+            std::swap( Sum, Num[i] );
             Sum += Num[i];
         }
         
-        for (T Iterator = Begin; Iterator != End; ++Iterator)
+        for (iterator Iterator = Begin; Iterator != End; ++Iterator)
         {
             Buffer[Num[KE(*Iterator, Iteration)]++] = *Iterator;
         }
         
-        for (size_t i = 0; i < ArrayLength; ++i)
-        {
-            Begin[i] = Buffer[i];
-        }
+        std::copy(Buffer.begin(), Buffer.end(), Begin);
     }
 }
 
@@ -121,30 +93,35 @@ long long rand_long_long ()
     return rand();
 }
 
-string rand_string ()
+std::string rand_string ()
 {
-    string s = "";
+    std::string s = "";
     for (int i = 0; i < 5; ++i)
         s += ('a' + rand() % 20);
     return s;
 }
 
-pair<long long, long long> rand_pair_long_long ()
+std::pair<long long, long long> rand_pair_long_long ()
 {
-    return make_pair( rand(), rand() );
+    return std::make_pair( rand(), rand() );
 }
 
-double dt (time_t a, time_t b)
+double GetDeltaTime (time_t a, time_t b)
 {
     return double(b - a) / CLOCKS_PER_SEC;
 }
 
-vector <int> vi;
-vector <long long> vll;
-vector <string> vs;
-vector < pair<long long, long long> > vpll;
+std::vector <int> vi;
+std::vector <long long> vll;
+std::vector <std::string> vs;
+std::vector < std::pair<long long, long long> > vpll;
 
 int main ()
 {
+	std::pair<long long, long long> a[] = {std::make_pair(6, 0), std::make_pair(5, 0), std::make_pair(10, 0), std::make_pair(-177, 0), std::make_pair(454, 0), std::make_pair(00, 0), std::make_pair(157, 0)};
+	DigitalSort(a, a + 7, NumberKeyExtractor<std::pair<long long, long long> >(8));
+	for (int i = 0; i < 7; ++i)
+		std::cout << a[i].first << " " << a[i].second << std::endl;
+	
     return 0;
 }
