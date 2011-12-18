35176cfc096b18fda6cf64025f1a1103ce61b78c
MekaGem <mekagem@gmail.com>
2011-12-18 21:39:35 +0400

    Update dsort.cpp

--- dsort.cpp
+++ dsort.cpp
@@ -28,12 +28,12 @@ public:
 	}
 };
 
-class SKeyExtractor
+class StringKeyExtractor
 {
 private:
     size_t blockNum;
 public:
-    explicit SKeyExtractor(size_t KeyBlockNum = 2)
+    explicit StringKeyExtractor(size_t KeyBlockNum = 2)
     {
         blockNum = KeyBlockNum;
     }
@@ -42,27 +42,27 @@ public:
         return ((size_t)(Source[blockNum * 2 - 1 - KeyIndex * 2]) + (size_t(Source[blockNum * 2 - 1 - KeyIndex * 2 - 1]) << 8));
     }
     size_t GetBlockNum ()const
-	{
+    {
 		return blockNum;
-	}
+    }
 };
 
 template <typename iterator, typename Extractor>
-void DigitalSort (iterator Begin, iterator End, Extractor KE )
+void DigitalSort (iterator Begin, iterator End, Extractor KeyExtractor )
 {
     const size_t KeyBlockSize = (1<<16), ArrayLength = End - Begin;
     
     std::vector <__typeof(*Begin)> Buffer(ArrayLength);
     std::vector <size_t> Num, StartIndex;
     
-    for (size_t Iteration = 0; Iteration < KE.GetBlockNum(); ++Iteration)
+    for (size_t Iteration = 0; Iteration < KeyExtractor.GetBlockNum(); ++Iteration)
     {
         Num.resize(0);
         Num.resize(KeyBlockSize, 0);
         
         for (iterator Iterator = Begin; Iterator != End; ++Iterator)
         {
-            Num[KE(*Iterator, Iteration)]++;
+            Num[KeyExtractor(*Iterator, Iteration)]++;
         }
         
         //std::partial_sum(Num.begin(), Num.end() - 1, Num.begin() + 1);
@@ -76,7 +76,7 @@ void DigitalSort (iterator Begin, iterator End, Extractor KE )
         
         for (iterator Iterator = Begin; Iterator != End; ++Iterator)
         {
-            Buffer[Num[KE(*Iterator, Iteration)]++] = *Iterator;
+            Buffer[Num[KeyExtractor(*Iterator, Iteration)]++] = *Iterator;
         }
         
         std::copy(Buffer.begin(), Buffer.end(), Begin);
@@ -96,7 +96,7 @@ long long rand_long_long ()
 std::string rand_string ()
 {
     std::string s = "";
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < 6; ++i)
         s += ('a' + rand() % 20);
     return s;
 }
@@ -111,17 +111,54 @@ double GetDeltaTime (time_t a, time_t b)
     return double(b - a) / CLOCKS_PER_SEC;
 }
 
-std::vector <int> vi;
-std::vector <long long> vll;
-std::vector <std::string> vs;
-std::vector < std::pair<long long, long long> > vpll;
+bool mycmp (const std::pair<long long, long long> &a, const std::pair<long long, long long> &b)
+{
+	if (a.second != b.second) return a.second < b.second;
+	if (a.first != b.first) return a.first < b.first;
+	return false;
+}
+
+const int TESTLEN = 10000;
+
+std::vector <int> vi(TESTLEN);
+std::vector <long long> vll(TESTLEN);
+std::vector <std::string> vs(TESTLEN);
+std::vector < std::pair<long long, long long> > vpll(TESTLEN);
 
 int main ()
 {
-	std::pair<long long, long long> a[] = {std::make_pair(6, 0), std::make_pair(5, 0), std::make_pair(10, 0), std::make_pair(-177, 0), std::make_pair(454, 0), std::make_pair(00, 0), std::make_pair(157, 0)};
-	DigitalSort(a, a + 7, NumberKeyExtractor<std::pair<long long, long long> >(8));
-	for (int i = 0; i < 7; ++i)
-		std::cout << a[i].first << " " << a[i].second << std::endl;
+	generate(vi.begin(), vi.end(), rand_int);
+	generate(vll.begin(), vll.end(), rand_long_long);
+	generate(vs.begin(), vs.end(), rand_string);
+	generate(vpll.begin(), vpll.end(), rand_pair_long_long);
+
+	DigitalSort(vi.begin(), vi.end(), NumberKeyExtractor<int>(2));
+	DigitalSort(vll.begin(), vll.end(), NumberKeyExtractor<long long>(4));
+	DigitalSort(vs.begin(), vs.end(), StringKeyExtractor(3));
+	DigitalSort(vpll.begin(), vpll.end(), NumberKeyExtractor<std::pair<long long, long long> >(sizeof(std::pair<long long, long long>) / 2));
 	
+	std::cout << "Sorting int is: ";
+	if (prev_permutation(vi.begin(), vi.end()))
+		std::cout << "FAIL\n";
+	else
+		std::cout << "OK\n";
+	
+	std::cout << "Sorting long long is: ";
+	if (prev_permutation(vll.begin(), vll.end()))
+		std::cout << "FAIL\n";
+	else
+		std::cout << "OK\n";
+	
+	std::cout << "Sorting string is: ";
+	if (prev_permutation(vs.begin(), vs.end()))
+		std::cout << "FAIL\n";
+	else
+		std::cout << "OK\n";
+	
+	std::cout << "Sorting pair of long long is: ";
+	if (prev_permutation(vpll.begin(), vpll.end(), mycmp))
+		std::cout << "FAIL\n";
+	else
+		std::cout << "OK\n";
     return 0;
 }
