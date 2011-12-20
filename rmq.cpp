60971b66cebdbbc3f9400b756c070aaeb17553aa
MekaGem <mekagem@gmail.com>
2011-12-20 19:20:57 +0400

    Update rmq.cpp

--- rmq.cpp
+++ rmq.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cassert>
 
 template<typename ValueType,
 typename ModType,
@@ -29,7 +30,8 @@ private:
 			_Left->_Mod = _PushFunc(_Mod, _Left->_Mod);
 			_Right->_Mod = _PushFunc(_Mod, _Right->_Mod);
 			_Mod = _ModDefault;
-			_Value = _MergeFunc(_ModFunc(_Left->_Mod, _Left->_Value), _ModFunc(_Right->_Mod, _Right->_Value));
+			_Value = _MergeFunc(_ModFunc(_Left->_Mod, _Left->_Value),
+								_ModFunc(_Right->_Mod, _Right->_Value));
 		}
 	}
 public:
@@ -52,15 +54,13 @@ public:
 		
 		if (_End > _Begin + 1)
 		{
-			_Left = new IntervalTree(_Begin, (_Begin + _End - 1) / 2 + 1, ValueDefault, ModDefault);
-			_Right = new IntervalTree((_Begin + _End - 1) / 2 + 1, _End, ValueDefault, ModDefault);
+			size_t Middle = (_Begin + _End - 1) / 2 + 1;
+			_Left = new IntervalTree(_Begin, Middle, ValueDefault, ModDefault);
+			_Right = new IntervalTree(Middle, _End, ValueDefault, ModDefault);
 		}
 	}
 	ValueType GetValue (size_t Begin, size_t End)
-	{
-		if (Begin < _Begin || End > _End)
-			;//assert(0);
-			
+	{			
 		Push();
 		
 		if (Begin == _Begin && End == _End)
@@ -71,13 +71,11 @@ public:
 		else if (Begin >= _Right->_Begin)
 			return _Right->GetValue(Begin, End);
 		else
-			return _MergeFunc(_Left->GetValue(Begin, _Left->_End), _Right->GetValue(_Right->_Begin, _End));
+			return _MergeFunc(_Left->GetValue(Begin, _Left->_End), 
+							  _Right->GetValue(_Right->_Begin, End));
 	}
 	void UpdateValue (size_t Begin, size_t End, ModType Mod)
-	{
-		if (Begin < _Begin || End > _End)
-			;//assert(0);
-		
+	{	
 		Push();
 		
 		if (Begin == _Begin && End == _End)
@@ -85,18 +83,20 @@ public:
 			_Mod = _PushFunc(Mod, _Mod);
 			return;
 		}		
-		
+
 		if (End <= _Left->_End)
 			_Left->UpdateValue(Begin, End, Mod);
 		else if (Begin >= _Right->_Begin)
-			return _Right->UpdateValue(Begin, End, Mod);
+			_Right->UpdateValue(Begin, End, Mod);
 		else
 		{
 			_Left->UpdateValue(Begin, _Left->_End, Mod);
-			_Right->UpdateValue(_Right->_Begin, _End, Mod);
+			_Right->UpdateValue(_Right->_Begin, End, Mod);
 		}
 		
-		_Value = _MergeFunc(_ModFunc(_Left->_Mod, _Left->_Value), _ModFunc(_Right->_Mod, _Right->_Value));
+		
+		_Value = _MergeFunc(_ModFunc(_Left->_Mod, _Left->_Value),
+							_ModFunc(_Right->_Mod, _Right->_Value));
 	}
 };
 
@@ -118,11 +118,64 @@ public:
 	}
 };
 
+class IntervalTreeChecker
+{
+private:
+	int *_a;
+	size_t _Size;
+public:
+	explicit IntervalTreeChecker (size_t Size)
+	{
+		_Size = Size;
+		_a = new int[_Size];
+		for (size_t i = 0; i < _Size; ++i)
+			_a[i] = 0;
+	}
+	~IntervalTreeChecker ()
+	{
+		delete [] _a;
+	}
+	void UpdateValue (size_t Begin, size_t End, int Value)
+	{
+		for (size_t i = Begin; i < End; ++i)
+			_a[i] += Value;
+	}
+	int GetValue (size_t Begin, size_t End)
+	{
+		int Max = _a[Begin];
+		for (size_t i = Begin; i < End; ++i)
+			Max = std::max(Max, _a[i]);
+		return Max;
+	}
+};
+
 int main ()
 {
-	IntervalTree<int, int, PLUS, MAX, PLUS > r(0, 4, int(NULL), int(NULL));
-	r.UpdateValue(1, 4, 1);
-	r.UpdateValue(3, 4, -1);
-	std::cout << r.GetValue(0, 3);
+	const size_t TestNum = 100000, TestSize = 1000;
+	
+	IntervalTree<int, int, PLUS, MAX, PLUS > r(0, TestSize, int(NULL), int(NULL));
+	IntervalTreeChecker InChk(TestSize);
+	for (size_t t = 0; t < TestNum; ++t)
+	{
+		int a = std::rand() % TestSize;
+		int b = a + std::rand() % (TestSize - a) + 1;
+		assert(a < b);
+		assert(b <= TestSize);
+		assert(a >= 0);
+		
+		int x = std::rand() % TestSize - (TestSize / 2);
+		if (std::rand() % 2 == 0)
+		{
+			r.UpdateValue(a, b, x);
+			InChk.UpdateValue(a, b, x);
+		}
+		else
+		{
+			int resA, resB;
+			resA = r.GetValue(a, b);
+			resB = InChk.GetValue(a, b);
+			assert(resA == resB);
+		}
+	}
 	return 0;
 }
