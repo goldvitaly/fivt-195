2f21548d78a459029e6bd83f324c401b6362a2f3
Ivan Smirnov <ifsmirnov@yandex.ru>
2011-12-26 12:42:58 +0300

    добавлен readme и код, помогающий работать с бинарными файлами. в myio.h убран using namespase std;

--- myio.h
+++ myio.h
@@ -9,21 +9,20 @@
 #include <cstring>
 #include <cassert>
 #include <iostream>
-using namespace std;
 
 template<typename T>
-T readInteger(istream& in)
+T readInteger(std :: istream& in)
 {
 	T res = 0;
 	for (size_t i = 0; i < sizeof(T); i++)
 		res = (res << 8) | (in.get());
 	return res;
 }
-int readInt(istream& in)
+int readInt(std :: istream& in)
 {
 	return readInteger<int>(in);
 }
-long long readI64(istream& in)
+long long readI64(std :: istream& in)
 {
 	return readInteger<long long>(in);
 }
@@ -40,41 +39,41 @@ long long readI64(char* s)
 }
 
 template<typename T>
-void writeInteger (ostream& out, T x)
+void writeInteger (std :: ostream& out, T x)
 {
 	out.write((char*)x, sizeof(T));
 }
-void writeInt(ostream& out, int x)
+void writeInt(std :: ostream& out, int x)
 {
 	writeInteger<int>(out, x);
 }
-void writeI64(ostream& out, long long x)
+void writeI64(std :: ostream& out, long long x)
 {
 	writeInteger<long long>(out, x);
 }
 
-streamoff fileSize(iostream& st)
+std :: streamoff fileSize(std :: iostream& st)
 {
-	streampos tmp = st.tellg();
+	std :: streampos tmp = st.tellg();
 	st.seekg(0, st.end);
-	streampos end = st.tellg();
+	std :: streampos end = st.tellg();
 	st.seekg(0, st.beg);
-	streampos beg = st.tellg();
+	std :: streampos beg = st.tellg();
 	
 	st.seekg(tmp);
 	return end - beg;
 }
 
-int peekInt(istream &st)
+int peekInt(std :: istream &st)
 {
 	int t = readInt(st);
 	st.seekg(-4, st.cur);
 	return t;
 }
 
-void printFile(istream& st, streampos b, streampos e, ostream& out)
+void printFile(std :: istream& st, std :: streampos b, std :: streampos e, std :: ostream& out)
 {
-	streampos tmp = st.tellg();
+	std :: streampos tmp = st.tellg();
 	if (e == -1)
 	{
 		st.seekg(0, st.end);
@@ -87,26 +86,26 @@ void printFile(istream& st, streampos b, streampos e, ostream& out)
 	st.seekg(b*4, st.beg);
 	for (; b != e; b += 1)
 		out << readInt(st) << " ";
-	out << endl;
+	out << std :: endl;
 	
 	st.seekg(tmp);
 }
-void printFile (istream& st)
+void printFile (std :: istream& st)
 {
-	printFile(st, 0, -1, cout);
+	printFile(st, 0, -1, std :: cout);
 }
-void printFile(istream& st, streampos b, streampos e)
+void printFile(std :: istream& st, std :: streampos b, std :: streampos e)
 {
-	printFile(st, b, e, cout);
+	printFile(st, b, e, std :: cout);
 }
-void printFile(istream& st, ostream& out)
+void printFile(std :: istream& st, std :: ostream& out)
 {
 	printFile(st, 0, -1, out);
 }
 void copy (const char* src, const char* dst)
 {
-	fstream in (src, ios :: in | ios :: binary);
-	fstream out (dst, ios :: out | ios :: binary);
+	std :: fstream in (src, std :: ios :: in | std :: ios :: binary);
+	std :: fstream out (dst, std :: ios :: out | std :: ios :: binary);
 	
 	int sz = fileSize(in);
 	for (int i = 0; i < sz; i++)
