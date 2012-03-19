071d990306790cafa8974398b19ede2ced2b6217
Alexey Dmitriev <riadwaw@gmail.com>
2012-03-19 12:11:17 +0400

    add tests

---
+++ TrivialStronglyConnectedComponents.hpp
@@ -0,0 +1,44 @@
+#ifndef TRIVIALSTRONGLYCONNECTEDCOMPONENTS_HPP
+#define TRIVIALSTRONGLYCONNECTEDCOMPONENTS_HPP
+#include "Graph.hpp"
+#include "StronglyConnectedComponentsInfo.hpp"
+#include <algorithm>
+#include <limits>
+#include <stack>
+#include <vector>
+class TrivialStronglyConnectedComponents{
+public:
+	explicit TrivialStronglyConnectedComponents(const Graph& graph): graph(graph) {}
+	std::vector<size_t> getColoring() {
+		int n = graph.size();
+		possible.assign(n,std::vector<char>(n, false));
+		for(int i = 0; i < n;++i){
+			dfs(i, i);
+		}
+		std::vector<size_t> v;
+		for(int i = 0; i < n; ++i){
+			v.push_back(i);
+		}
+		for(int i = 0; i < n; ++i){
+			for(int j = 0; j < n; ++j){
+				if(possible[i][j] && possible[j][i]){
+					v[j] = v[i];
+				}
+			}
+		}
+		return v;
+	}
+private:
+	void dfs(size_t start, size_t v){
+		possible[start][v] = true;
+		for(const auto& next: graph.getIncidents(v)){
+			if(!possible[start][next])
+				dfs(start, next);
+		}
+	}
+	std::vector<std::vector<char> > possible;
+	const Graph& graph;
+};
+
+#endif /* TRIVIALSTRONGLYCONNECTEDCOMPONENTS_HPP */
+
--- main.cpp
+++ main.cpp
@@ -2,35 +2,56 @@
 #include "Incidents.hpp"
 #include "SetIncidents.hpp"
 #include "StronglyConnectedComponents.hpp"
+#include "TrivialStronglyConnectedComponents.hpp"
 #include "VectorIncidents.hpp"
 #include "StronglyConnectedComponentsInfo.hpp"
 #include <cstdlib>
 #include <iostream>
-
+#include <set>
+#include <map>
 using namespace std;
-int main() {
-	Graph graph;
-	for(int i = 0; i < 5; ++i){
-		graph.addVertex(new SetIncidents());
-	}
-	graph.addEdge(3, 4);
-	graph.addEdge(4, 3);
-	graph.addEdge(1, 4);
-	graph.addEdge(1, 2);
-	graph.addEdge(2, 0);
-	graph.addEdge(0, 1);
-	StronglyConnectedComponentsInfo scc (StronglyConnectedComponents(graph).getComponents());
-	
-	std::cout<<scc.getComponentsNumber()<<endl;
-	
-	auto components = scc.allComponents();
-	for(auto component: components){
-		for(auto vertex: component){
-			cout<<vertex<<' ';
-		}
-		cout<<endl;
-	}
 
+typedef std::vector<size_t> Coloring;
+
+Graph genGraph(int mask){
+	Graph graph;
+	graph.addVertex(new VectorIncidents());
+	graph.addVertex(new SetIncidents());
+	graph.addVertex(new VectorIncidents());
+	graph.addVertex(new SetIncidents());
+	for(int i=0; i < 4; ++i){
+		for(int j = 0; j < 4; ++j){
+			int curEdge = 4 * i + j;
+			if(mask & (1 << curEdge)){
+				graph.addEdge(i, j);
+			}
+		}
+	}
+	return graph;
+}
+bool checkColoringsEqual(Coloring a, Coloring b){
+	assert(a.size()==b.size());
+	map<size_t, set<size_t> > m;
+	for(size_t i = 0; i < a.size(); ++i){
+		m[a[i]].insert(b[i]);
+	}
+	for(auto& s: m){
+		if(s.second.size() != 1)
+			return false;
+	}
+	return true;
+}
+bool testGraph(const Graph& graph){
+	Coloring received = StronglyConnectedComponents(graph).getComponents().ids();
+	Coloring expected = TrivialStronglyConnectedComponents(graph).getColoring();
+	return checkColoringsEqual(received, expected);
+}
+int main() {
+	for(int i = 0; i < (1 << 16); ++i){
+		if(!testGraph(genGraph(i))){
+			cerr<<"Test failed on 4-vertex graph";
+			exit (1);
+		}
+	}
 	return 0;
 }
-
