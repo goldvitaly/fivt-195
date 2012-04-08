ace0b78a786eda2ab7f627b1b1945d51e9e13d9e
MekaGem <mekagem@gmail.com>
2012-04-09 01:26:10 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include "graph.hpp"
+#include "graph_algorithm.hpp"
 #include <iostream>
 
 int main ()
 {	
     Graph G;
-	G.AddVertex<MyIncident>(4);
-	/*G.AddIncident(0, 1);
+	G.AddVertex<VIntIncident>(4);
+	
+	G.AddIncident(0, 1);
 	G.AddIncident(0, 2);
 	G.AddIncident(1, 3);
 	G.AddIncident(3, 1);
 	G.AddIncident(2, 4);
-	G.AddIncident(4, 2);*/
+	G.AddIncident(4, 2);
 
-    //	GraphAlgorithm ga;
-	//std::vector<size_t> ord = ga.FindStronglyConnectedComponents(G);
+    	GraphAlgorithm ga;
+	std::vector<size_t> ord = ga.FindStronglyConnectedComponents(G);
 	
-	//for (size_t i = 0; i < ord.size(); ++i)
-	//	std::cout << ord[i] << " ";
+	for (auto i : ord)
+		std::cout << i << " ";
 
 	return 0;
 }
