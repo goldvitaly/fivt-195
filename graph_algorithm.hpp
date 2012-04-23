83e22edb926c87750a8b9fb3c78ed62804b27e95
MekaGem <mekagem@gmail.com>
2012-04-23 09:57:31 +0400

    Update graph_algorithm.hpp

--- graph_algorithm.hpp
+++ graph_algorithm.hpp
@@ -14,6 +14,8 @@ public:
         _Used.assign(G.VertexNum(), false);
         _Comp.assign(G.VertexNum(), 0);
         _Tin.assign(G.VertexNum(), 0);
+        _TinMin.assign(G.VertexNum(), 0);
+        _InProcess.assign(G.VertexNum(), 0);
         _Stack.clear();
         _Timer = 0;
         _Color = 0;
@@ -26,37 +28,37 @@ public:
     }
 private:
     size_t _Timer, _Color;
-    std::vector<size_t> _Comp, _Tin;
+    std::vector<size_t> _Comp, _Tin, _TinMin;
     std::vector<bool> _Used;
     std::vector<size_t> _Stack;
+    std::vector<bool> _InProcess;
     
     void StronglyConnectedComponentsDFS (const Graph& G, size_t v)
     {
         _Used[v] = true;
         _Stack.push_back(v);
-        _Tin[v] = _Timer++;
+        _Tin[v] = _TinMin[v] = _Timer++;
+        _InProcess[v] = true;
         
-        bool IsRoot = true;
-        const Incident* inc = G.GetIncident(v);
         for (auto u : *G.GetIncident(v))
         {
             if (!_Used[u])
-                StronglyConnectedComponentsDFS(G, u);
-            if (_Tin[v] > _Tin[u])
             {
-                _Tin[v] = _Tin[u];
-                IsRoot = false;
+                StronglyConnectedComponentsDFS(G, u);
+                _TinMin[v] = std::min(_TinMin[v], _TinMin[u]);
             }
+            else if (_InProcess[u])
+                _TinMin[v] = std::min(_TinMin[v], _Tin[u]);
         }
         
-        if (IsRoot)
+        if (_Tin[v] == _TinMin[v])
         {
             size_t u;
             do
             {
                 u = _Stack.back();
                 _Stack.pop_back();
-                
+                _InProcess[u] = false;
                 _Comp[u] = _Color;                
                 
             } while (u != v);
