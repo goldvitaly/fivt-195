fa1cab6248526d1f2603a5f0f22801111acef9b0
amashrabov <amashrabov@gmail.com>
2012-05-03 23:17:18 +0400

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -11,7 +11,7 @@ class Graph
 private:
 	std::vector< std::unique_ptr< Vertex<TWeight> > > graph;
 public:
-   	void addEdge(size_t from, size_t to, const TWeight& weight = Tweight())
+	void addEdge(size_t from, size_t to, const TWeight& weight = Tweight())
 	{
 		graph[from]->addNeighbour(Edge<TWeight>(to, weight));
 	}
