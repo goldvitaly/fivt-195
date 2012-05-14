8308c94e208404a9e74421fa4f02d3415e0f4e45
MekaGem <mekagem@gmail.com>
2012-05-14 11:49:49 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ std::vector<bool> used;
 void dfs (int v, const Graph &G)
 {
     used[v] = true;
-    for (auto u : G.GetIncident(v))
+	for (auto u : G.GetIncident(v))
     {
         if (!used[u])
             dfs(u, G);
@@ -23,7 +23,7 @@ bool test (size_t vertexNum, size_t edgeNum)
     {
         G.AddVertex<IncidentType>();
     }
-    
+
     for (size_t i = 0; i < edgeNum; ++i)
     {
         size_t from, to;
@@ -33,38 +33,38 @@ bool test (size_t vertexNum, size_t edgeNum)
     }
 
     GraphAlgorithm ga;
-	std::vector<size_t> ord = ga.FindStronglyConnectedComponents(G);
-	
-	for (size_t v = 0; v < vertexNum; ++v)
-	{
-	    for (size_t u = v + 1; u < vertexNum; ++u)
-	    {
-	        bool check = true;
-	        used.assign(vertexNum, 0);
-	        dfs(v, G);
-	        if (!used[u]) check = false;
-	        
-	        used.assign(vertexNum, 0);
-	        dfs(u, G);
-	        if (!used[v]) check = false;
-	        
+    std::vector<size_t> ord = ga.FindStronglyConnectedComponents(G);
+
+    for (size_t v = 0; v < vertexNum; ++v)
+    {
+        for (size_t u = v + 1; u < vertexNum; ++u)
+        {
+            bool check = true;
+            used.assign(vertexNum, 0);
+            dfs(v, G);
+            if (!used[u]) check = false;
+
+            used.assign(vertexNum, 0);
+            dfs(u, G);
+            if (!used[v]) check = false;
+
             if ((ord[v] == ord[u]) != (check))
                 return false;
-	    }
-	}
-	
-	return true;
+        }
+    }
+
+    return true;
 }
 
 int main ()
-{	
+{
     const size_t testNum = 1000000;
     const size_t testN = 30;
     const size_t testM = 100;
     const size_t testSrand = 333;
-    
+
     srand(testSrand);
-    
+
     for (size_t t = 0; t < testNum; ++t)
     {
         //Vector of Int
@@ -73,7 +73,7 @@ int main ()
             std::cerr << "test #" << t + 1 << " ok" << std::endl;
         else
             std::cerr << "test #" << t + 1 << " failed" << std::endl;
-            
+
         //Vector of Bool
         std::cerr << "VBoolIncident ";
         if (test<VBoolIncident>(testN, testM))
@@ -81,6 +81,6 @@ int main ()
         else
             std::cerr << "test #" << t + 1 << " failed" << std::endl;
     }
-    
-	return 0;
+
+    return 0;
 }
