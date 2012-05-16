b587cf7ca1a27fab58e7a4d152a0f8223d99303b
amashrabov <amashrabov@gmail.com>
2012-05-16 19:37:26 +0400

    Floyd is true algo

--- Test_dijkstra.h
+++ Test_dijkstra.h
@@ -25,7 +25,7 @@ inline void makeGraph(size_t numVertices, size_t numEdges, int maxWeight, Graph<
     }
 }
 
-inline void trueAlgo(TypeTestGraph& tG, size_t from, std::vector<int>* outDist, std::vector<char>* outIsReached)
+inline void trueAlgoDijkstra(TypeTestGraph& tG, size_t from, std::vector<int>* outDist, std::vector<char>* outIsReached)
 {
     size_t n = tG.size();
     std::vector<char> mark(n, false);
@@ -56,31 +56,65 @@ inline void trueAlgo(TypeTestGraph& tG, size_t from, std::vector<int>* outDist, 
     *outIsReached = mark;
 }
 
+inline void trueAlgoFloyd(TypeTestGraph& tG, size_t from, std::vector<int>* outDist, std::vector<char>* outIsReached)
+{
+    size_t n = tG.size();
+    std::vector< std::vector<char> > isReached(n);
+    for (size_t i = 0; i < n; i++)
+		isReached[i].assign(n, false);
+    std::vector< std::vector<int> > d(n);
+    for (size_t i = 0; i < n; i++)
+	{
+		d[i].assign(n, std::numeric_limits<int>::max());
+		d[i][i] = 0;
+		isReached[i][i] = true;
+	}
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < tG[i].size(); j++)
+		{
+			int to = tG[i][j].first, weight = tG[i][j].second;
+			d[i][to] = std::min(d[i][to], weight);
+			isReached[i][to] = true;
+		}
+	for (size_t k = 0; k < n; k++)
+    {
+        for (size_t i = 0; i < n; i++)
+			for (size_t j = 0; j < n; j++)
+				if (isReached[i][k] && isReached[k][j])
+				{
+					d[i][j] = std::min(d[i][j], d[i][k] + d[k][j]);
+					isReached[i][j] = true;
+				}
+	}
+	for (size_t i = 0; i < n; i++)
+	{
+		(*outDist)[i] = d[from][i];
+		(*outIsReached)[i] = isReached[from][i];
+	}
+}
 inline bool runTestDijkstra(size_t numVertices, size_t numEdges, int maxWeight)
 {
     time_t start = clock();
     Graph<int> G;
     TypeTestGraph tG;
     makeGraph(numVertices, numEdges, maxWeight, G, tG);
-    std::vector<int> dijkstraDist(numVertices);
-    std::vector<char> dijkstraIsReached(numVertices);
     std::vector<int> trueDist(numVertices);
     std::vector<char> trueIsReached(numVertices);
 
     Dijkstra<int, int> dijkstra(G);
     size_t from = rand() % numVertices;
     ShortestPathInfo<int> pathInfo =  dijkstra.calcDist(from);
-    std::pair<std::vector<int>, std::vector<char>> tmpRes = pathInfo.getLengths();
-    dijkstraDist = tmpRes.first;
-    dijkstraIsReached = tmpRes.second;
-    std::cout << numVertices << " " << numEdges << " ";
+	std::cout << numVertices << " " << numEdges << " ";
     std::cout << (double (clock()) - start) / CLOCKS_PER_SEC << " ";
-    trueAlgo(tG, from, &trueDist, &trueIsReached);
-    bool res = true;
-    for (size_t i = 0; i < numVertices; ++i)
+   
+    //trueAlgoDijkstra(tG, from, &trueDist, &trueIsReached);
+    trueAlgoFloyd(tG, from, &trueDist, &trueIsReached);
+ 
+	bool res = true;
+	for (size_t i = 0; i < numVertices; ++i)
         if (!trueIsReached[i])
         {
-            if (dijkstraIsReached[i])
+            if (pathInfo.isReach(i))
             {
                 res = false;
                 break;
@@ -88,7 +122,7 @@ inline bool runTestDijkstra(size_t numVertices, size_t numEdges, int maxWeight)
         }
         else
         {
-            if (dijkstraDist[i] != trueDist[i])
+            if (pathInfo.getLength(i) != trueDist[i])
             {
                 res = false;
                 break;
@@ -100,8 +134,8 @@ inline bool runTestDijkstra(size_t numVertices, size_t numEdges, int maxWeight)
 
 inline void testDijkstra()
 {
-    srand(31518905);
-    const size_t qTests = 20, maxN = 500, maxM = 5000, maxWeight = 100000;
+    srand(51518905);
+    const size_t qTests = 20, maxN = 100, maxM = 1000, maxWeight = 10000;
     bool res;
     std::cout << "numVertices numEdges time(sec) result" << std::endl;
     for (size_t i = 0; i < qTests; i++)
@@ -109,7 +143,7 @@ inline void testDijkstra()
         srand(rand());
         size_t numVertices = (rand() % maxN) + 1;
         size_t numEdges = (rand() % maxM) + 1;
-        size_t curMaxWeight = (rand() % maxWeight);
+        size_t curMaxWeight = (rand() % maxWeight) + 1;
         res = runTestDijkstra(numVertices, numEdges, curMaxWeight);
         if (res)
             std::cout << "OK";
